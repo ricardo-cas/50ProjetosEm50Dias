162a69bbade9a999dca3e39f5b8cbd3a3dd871fb
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-02-17 11:05:38 -0300

    função dos botões anterior e proximo finalizada

--- progress/assets/js/script.js
+++ progress/assets/js/script.js
@@ -30,9 +30,26 @@ prev.addEventListener("click", () => {
 function update() {
   circles.forEach((circle, idx) => {
     if (idx < currentActive) {
+      // adicionando a classe "active, no próximo circulo sempre que o botão for clicado"
       circle.classList.add("active");
     } else {
+      // removendo a classe "active, no circulo anterior sempre quando botão for clicado"
       circle.classList.remove("active");
     }
   });
+  //  Fazendo a barra de progresso aparecer
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  // criando a validação dos botões para eles funcionarem.
+  if (currentActive == 1) {
+    prev.disabled = true;
+  } else if (currentActive == circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
